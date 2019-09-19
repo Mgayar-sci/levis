aa07b31057329992d6ffcfae81a81170de9de8f2
ahmed <ahmed.abdelkarim@nozol.com>
2019-09-19 14:31:22 +0200

    Updates

---
+++ precache-manifest.c17b5ec14532eb512fb8ab95b957c7da.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1a4b310182815a4cdff8cf44375fda",
+    "url": "/Ahmed-Abdel-karim/levis/index.html"
+  },
+  {
+    "revision": "486b9be7fd8d98ed8be5",
+    "url": "/Ahmed-Abdel-karim/levis/static/css/main.b80272fb.chunk.css"
+  },
+  {
+    "revision": "43715b9502cd37534b17",
+    "url": "/Ahmed-Abdel-karim/levis/static/js/2.358a9133.chunk.js"
+  },
+  {
+    "revision": "486b9be7fd8d98ed8be5",
+    "url": "/Ahmed-Abdel-karim/levis/static/js/main.24f54356.chunk.js"
+  },
+  {
+    "revision": "7b36bbd37f26c341b727",
+    "url": "/Ahmed-Abdel-karim/levis/static/js/runtime~main.99b263d1.js"
+  },
+  {
+    "revision": "bfd3ccbe60a1f7d20ce38b78c5f8150a",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/ad.bfd3ccbe.jpg"
+  },
+  {
+    "revision": "71b351d2b778248c34f68f1d2d1f59e1",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/banner.71b351d2.png"
+  },
+  {
+    "revision": "3084ce8a841f4c540c3051330d6fb64c",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/branches.3084ce8a.png"
+  },
+  {
+    "revision": "9a19656729242ba515d1046e11635109",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/final.9a196567.png"
+  },
+  {
+    "revision": "7996a1b4bb56a7231204099a3c3afec0",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/final2.7996a1b4.png"
+  },
+  {
+    "revision": "32b351c15a228f8e49837060e6c40640",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/kids.32b351c1.png"
+  },
+  {
+    "revision": "06787a00ae72ec500c11402f8cb6aff2",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/men.06787a00.png"
+  },
+  {
+    "revision": "2865c28b0121554c03e04b0315016471",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/p1.2865c28b.png"
+  },
+  {
+    "revision": "59ba42e060c4ba4c121f6a323a6c9bf0",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/p2.59ba42e0.png"
+  },
+  {
+    "revision": "4c8f47eec2ad1cd56dd26ca2535c7f16",
+    "url": "/Ahmed-Abdel-karim/levis/static/media/women.4c8f47ee.png"
+  }
+]);
